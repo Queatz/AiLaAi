0b83fe1ba20fd725eb2f413f10300472924bef7a
Jacob Ferrero <jacobaferrero@gmail.com>
2024-02-22 23:42:25 +0700

    Set up VideoSDK

--- android/app/src/main/java/com/queatz/ailaai/CallActivity.kt
+++ android/app/src/main/java/com/queatz/ailaai/CallActivity.kt
@@ -1,12 +1,15 @@
 package com.queatz.ailaai
 
 import android.app.PictureInPictureParams
+import android.content.Context
 import android.content.Intent
+import android.media.projection.MediaProjectionManager
 import android.os.Build
 import android.os.Bundle
 import android.util.Rational
 import androidx.activity.addCallback
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -15,16 +18,53 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import app.ailaai.api.group
+import app.ailaai.api.groupCall
 import app.ailaai.api.me
 import com.queatz.ailaai.call.CallScreen
 import com.queatz.ailaai.data.api
 import com.queatz.ailaai.extensions.rememberIsInPipMode
 import com.queatz.ailaai.extensions.rememberStateOf
 import com.queatz.ailaai.ui.theme.AiLaAiTheme
+import com.queatz.db.GroupExtended
 import com.queatz.db.Person
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import live.videosdk.rtc.android.Meeting
+import live.videosdk.rtc.android.Participant
+import live.videosdk.rtc.android.Stream
+import live.videosdk.rtc.android.VideoSDK
+import live.videosdk.rtc.android.listeners.MeetingEventListener
+import live.videosdk.rtc.android.listeners.ParticipantEventListener
+import org.json.JSONObject
+import org.webrtc.AudioTrack
+import org.webrtc.MediaStreamTrack
+import org.webrtc.VideoTrack
 
+data class GroupCallParticipant(
+    val participant: Participant,
+    val stream: MediaStreamTrack,
+    val kind: String
+)
+
+data class GroupCall(
+    val group: GroupExtended,
+    val meeting: Meeting,
+    val localVideo: VideoTrack? = null,
+    val localAudio: AudioTrack? = null,
+    val localShare: VideoTrack? = null,
+    val pinnedStream: VideoTrack? = null,
+    val streams: List<GroupCallParticipant> = emptyList()
+)
 
 class CallActivity : AppCompatActivity() {
+
+    private var meeting: Meeting? = null
+    private var enabledStreams = MutableStateFlow(emptySet<String>())
+    private val active = MutableStateFlow<GroupCall?>(null)
+    private val group = MutableStateFlow<GroupExtended?>(null)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -38,6 +78,7 @@ class CallActivity : AppCompatActivity() {
             AiLaAiTheme {
                 var me by rememberStateOf<Person?>(null)
                 val isInPipMode = rememberIsInPipMode()
+                val context = LocalContext.current
 
                 val groupId = remember {
                     when (intent?.action) {
@@ -62,6 +103,18 @@ class CallActivity : AppCompatActivity() {
                     return@AiLaAiTheme
                 }
 
+                LaunchedEffect(groupId) {
+                    api.group(groupId) {
+                        group.value = it
+                    }
+                }
+
+                LaunchedEffect(me, group) {
+                    if (me != null && group.value != null) {
+                        join(context, me!!, groupId)
+                    }
+                }
+
                 CompositionLocalProvider(LocalAppState provides AppState(me)) {
                     Box(
                         modifier = Modifier
@@ -69,7 +122,6 @@ class CallActivity : AppCompatActivity() {
                             .then(if (isInPipMode) Modifier.clip(MaterialTheme.shapes.large) else Modifier)
                             .background(MaterialTheme.colorScheme.background)
                     ) {
-
                         if (me != null) {
                             CallScreen(
                                 groupId,
@@ -87,6 +139,166 @@ class CallActivity : AppCompatActivity() {
     override fun onStop() {
         super.onStop()
         finish()
+    }
+
+    private suspend fun join(context: Context, me: Person, groupId: String) {
+        api.groupCall(groupId) {
+            VideoSDK.config(it.token)
+            meeting = VideoSDK.initMeeting(
+                context,
+                it.call.room!!,
+                me.name ?: context.getString(R.string.someone),
+                true,
+                true,
+                null,
+                null,
+                true,
+                emptyMap(),
+                JSONObject()
+            )
+
+            val meeting = meeting ?: return@groupCall
+
+            meeting.localParticipant.addEventListener(object : ParticipantEventListener() {
+                override fun onStreamEnabled(stream: Stream?) {
+                    enabledStreams.update {
+                        it + (stream!!.kind as String)
+                    }
+                    if (stream!!.kind == "video") {
+                        // todo: set stream kind?
+                        active.value = active.value!!.copy(localVideo = stream.track as VideoTrack)
+                    } else if (stream.kind == "audio") {
+                        // todo: set stream kind?
+                        active.value = active.value!!.copy(localAudio = stream.track as AudioTrack)
+                    } else if (stream.kind == "share") {
+                        // todo: set stream kind?
+                        active.value = active.value!!.copy(localShare = stream.track as VideoTrack)
+                    }
+                }
+
+                override fun onStreamDisabled(stream: Stream?) {
+                    enabledStreams.update {
+                        it - (stream!!.kind as String)
+                    }
+                    if (stream!!.kind == "video") {
+                        active.value = active.value!!.copy(localVideo = null)
+                    } else if (stream.kind == "audio") {
+                        active.value = active.value!!.copy(localAudio = null)
+                    } else if (stream.kind == "share") {
+                        active.value = active.value!!.copy(localShare = null)
+                    }
+                }
+            })
+
+            meeting.addEventListener(object : MeetingEventListener() {
+                override fun onMeetingJoined() {
+
+                }
+
+                override fun onMeetingLeft() {
+
+                }
+
+                override fun onParticipantJoined(participant: Participant) {
+                    participant.addEventListener(object : ParticipantEventListener() {
+                        override fun onStreamEnabled(stream: Stream?) {
+                            active.value = active.value!!.copy(
+                                streams = active.value!!.streams + GroupCallParticipant(
+                                    participant,
+                                    stream!!.track,
+                                    stream!!.kind
+                                )
+                            )
+                        }
+
+                        override fun onStreamDisabled(stream: Stream?) {
+                            active.value = active.value!!.copy(
+                                streams = active.value!!.streams.filter {
+                                    it.stream != stream!!.track
+                                }
+                            )
+                        }
+                    })
+                }
+
+                override fun onParticipantLeft(participant: Participant) {
+                    active.value = active.value!!.copy(
+                        streams = active.value!!.streams.filter {
+                            it.participant != participant
+                        }
+                    )
+                }
+
+                override fun onError(error: JSONObject?) {
+
+                }
+            })
+
+            meeting.join()
+
+            active.value = GroupCall(
+                group.value!!,
+                meeting
+            )
+        }
+    }
+
+    fun toggleCamera() {
+        val meeting = meeting ?: return
+        if (enabled("video")) {
+            meeting.disableWebcam()
+        } else {
+            meeting.enableWebcam()
+        }
+    }
+
+    fun toggleMic() {
+        val meeting = meeting ?: return
+        if (enabled("audio")) {
+            meeting.muteMic()
+        } else {
+            meeting.unmuteMic()
+        }
+    }
+
+    fun toggleScreenShare() {
+        val meeting = meeting ?: return
+        if (enabled("share")) {
+            meeting.disableScreenShare()
+        } else {
+            val mediaProjectionManager = getSystemService(MediaProjectionManager::class.java)
+            val startMediaProjection = registerForActivityResult(
+                ActivityResultContracts.StartActivityForResult()
+            ) { result ->
+                if (result.resultCode == RESULT_OK) {
+                    meeting.enableScreenShare(result.data)
+                } else {
+                    // todo show error
+                }
+            }
+
+            startMediaProjection.launch(mediaProjectionManager.createScreenCaptureIntent())
+        }
+    }
+
+    fun togglePin(stream: VideoTrack?) {
+        active.value = active.value!!.copy(
+            pinnedStream = stream.takeIf { active.value!!.pinnedStream != stream }
+        )
+    }
+
+    fun end() {
+        meeting?.leave()
+        meeting = null
+        active.value = null
+    }
+
+
+    private fun enabled(stream: String): Boolean {
+        val meeting = meeting ?: return false
+        return meeting.localParticipant.streams.values.any {
+            it.kind == stream
+        } || enabledStreams.value.contains(stream)
     }
 
     private fun enterPip() {
--- android/app/src/main/java/com/queatz/ailaai/services/Calls.kt
+++ android/app/src/main/java/com/queatz/ailaai/services/Calls.kt
@@ -3,8 +3,19 @@ package com.queatz.ailaai.services
 import android.content.Context
 import android.content.Intent
 import androidx.core.os.bundleOf
+import app.ailaai.api.calls
 import com.queatz.ailaai.CallActivity
 import com.queatz.ailaai.CallActivity.Companion.GROUP_ID_EXTRA
+import com.queatz.ailaai.data.api
+import com.queatz.db.Call
+import com.queatz.push.CallStatusPushData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 val calls by lazy {
     Calls()
@@ -14,8 +25,29 @@ class Calls {
 
     private lateinit var context: Context
 
+    val calls = MutableStateFlow<List<Call>>(emptyList())
+
+    val scope = CoroutineScope(Dispatchers.Default)
+
     fun init(context: Context) {
         this.context = context
+
+        scope.launch {
+            reload()
+            push.events.filter {
+                it is CallStatusPushData
+            }.collectLatest {
+                reload()
+            }
+        }
+    }
+
+    suspend fun reload() {
+        api.calls { calls ->
+            this.calls.update {
+                calls
+            }
+        }
     }
 
     fun join(groupId: String) {
