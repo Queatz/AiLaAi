896e55d7abaed46add060dcfd53c53959a0d53fe
Jacob Ferrero <jacobaferrero@gmail.com>
2024-05-04 21:55:51 +0700

    Update active trades dialog ui

--- android/app/src/main/java/com/queatz/ailaai/trade/ActiveTradeItem.kt
+++ android/app/src/main/java/com/queatz/ailaai/trade/ActiveTradeItem.kt
@@ -6,15 +6,20 @@ import androidx.compose.material.icons.outlined.Check
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.queatz.ailaai.R
+import com.queatz.ailaai.extensions.bulletedString
+import com.queatz.ailaai.extensions.contactPhoto
 import com.queatz.ailaai.extensions.timeAgo
 import com.queatz.ailaai.me
+import com.queatz.ailaai.ui.components.GroupPhoto
 import com.queatz.ailaai.ui.theme.pad
 import com.queatz.db.TradeExtended
 
@@ -22,21 +27,26 @@ import com.queatz.db.TradeExtended
 fun ActiveTradeItem(trade: TradeExtended, onClick: () -> Unit) {
     val me = me
 
-     Card(
+     OutlinedCard(
          onClick = onClick,
          shape = MaterialTheme.shapes.large,
          modifier = Modifier
              .fillMaxWidth()
      ) {
          Row(
+             horizontalArrangement = Arrangement.spacedBy(1.pad),
              verticalAlignment = Alignment.CenterVertically,
              modifier = Modifier
                  .fillMaxWidth()
+                 .padding(1.5f.pad)
          ) {
+             GroupPhoto(
+                 photos = trade.people!!.filter { it.id != me?.id }.map { it.contactPhoto() },
+                 padding = 0.pad
+             )
              Column(
                  modifier = Modifier
                      .weight(1f)
-                     .padding(1.pad)
              ) {
                  Text(
                      trade.people!!.filter { it.id != me?.id }.map { it.name ?: stringResource(R.string.someone) }
@@ -44,10 +54,10 @@ fun ActiveTradeItem(trade: TradeExtended, onClick: () -> Unit) {
                      style = MaterialTheme.typography.titleLarge
                  )
                  Text(
-                     listOf(
+                     bulletedString(
                          trade.trade!!.createdAt!!.timeAgo(),
                          pluralStringResource(R.plurals.x_items, trade.inventoryItems!!.size, trade.inventoryItems!!.size)
-                     ).joinToString(" • ")
+                     )
                  )
              }
              if (trade.trade?.members?.any { it.person == me?.id && it.confirmed == true } == true) {
@@ -56,9 +66,7 @@ fun ActiveTradeItem(trade: TradeExtended, onClick: () -> Unit) {
                      null,
                      tint = MaterialTheme.colorScheme.primary,
                      modifier = Modifier
-                         .padding(
-                             horizontal = 2.pad
-                         )
+                         .padding(horizontal = 1.pad)
                  )
              }
          }
