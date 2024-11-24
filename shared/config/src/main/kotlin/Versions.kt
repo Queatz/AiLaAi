79b6f000c4481a83ab3b504292462eaea85f289e
Jacob Ferrero <jacobaferrero@gmail.com>
2024-11-24 21:58:48 +0700

    Update deps

--- shared/config/src/main/kotlin/Versions.kt
+++ shared/config/src/main/kotlin/Versions.kt
@@ -3,12 +3,12 @@ package com.ailaai.shared.config
 open class Versions {
     val kotlin = "2.0.21"
     val coroutines = "1.8.1"
-    val ktor = "3.0.0"
+    val ktor = "3.0.1"
     val choiceSdk = "0.3.0"
     val compose = "1.7.5"
     val serialization = "1.7.3"
     val datetime = "0.5.0"
     val logback = "1.4.12"
-    val arango = "7.5.1"
+    val arango = "7.13.0"
     val jackson = "2.15.3"
 }
