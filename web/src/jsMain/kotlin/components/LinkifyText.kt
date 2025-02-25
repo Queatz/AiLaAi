5d8e11f25a955bd439b05081cafcafcbb9cf372b
Jacob Ferrero <jacobaferrero@gmail.com>
2025-02-25 21:38:36 +0700

    Markdown already auto-links

--- web/src/jsMain/kotlin/components/LinkifyText.kt
+++ web/src/jsMain/kotlin/components/LinkifyText.kt
@@ -1,33 +1,6 @@
 package components
 
 import androidx.compose.runtime.Composable
-import com.queatz.db.splitByUrls
-import org.jetbrains.compose.web.attributes.ATarget
-import org.jetbrains.compose.web.attributes.target
-import org.jetbrains.compose.web.dom.A
-import org.jetbrains.compose.web.dom.Text
 
 @Composable
-fun LinkifyText(text: String) {
-    text.splitByUrls().forEach { (part, isUrl) ->
-        if (isUrl) {
-            A(
-                href = part.let {
-                    when {
-                        it.contains("@") && !it.contains("/") -> "mailto:$it"
-                        it.contains("://") -> it
-                        else -> "https://$it"
-                    }
-                }, {
-                    target(ATarget.Blank)
-                    onClick {
-                        it.stopPropagation()
-                    }
-                }) {
-                Text(part)
-            }
-        } else {
-            Markdown(part)
-        }
-    }
-}
+fun LinkifyText(text: String) = Markdown(text)
