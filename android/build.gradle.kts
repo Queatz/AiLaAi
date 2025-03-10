59f545b37a8635fceefb5092c9bb80bc933c4c34
Nate Ferrero <nate@ledger.ai>
2025-03-09 23:05:55 -0400

    upgrade to gradle 8.11.1

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.7.3")
+        classpath("com.android.tools.build:gradle:8.9.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
         classpath("com.huawei.agconnect:agcp:1.9.1.300")
         classpath("com.google.gms:google-services:4.4.2")
