2285e3f44e4d042ca76f389785aa0da998157f55
Jacob Ferrero <jacobaferrero@gmail.com>
2025-04-26 23:16:31 +0700

    Version bump 1.0.5

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         applicationId = "com.ailaai.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 104
-        versionName = "1.0.4"
+        versionCode = 105
+        versionName = "1.0.5"
 
         vectorDrawables {
             useSupportLibrary = true
