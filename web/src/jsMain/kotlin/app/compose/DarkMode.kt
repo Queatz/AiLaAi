78412deda1ba18fd69e8463f37bcaf545e950752
Jacob Ferrero <jacobaferrero@gmail.com>
2026-06-10 11:54:14 +0700

    Save latest dark mode

--- web/src/jsMain/kotlin/app/compose/DarkMode.kt
+++ web/src/jsMain/kotlin/app/compose/DarkMode.kt
@@ -3,18 +3,22 @@ package app.compose
 import androidx.compose.runtime.*
 import kotlinx.browser.window
 
+private var latestDarkMode = false
+
 @Composable
 fun rememberDarkMode(key: Any = Unit): Boolean {
-    val isDarkMode = remember { mutableStateOf(false) }
+    val isDarkMode = remember { mutableStateOf(latestDarkMode) }
 
     DisposableEffect(key) {
         // Check the current dark mode preference
         val darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)")
         isDarkMode.value = darkModeMediaQuery.matches
+        latestDarkMode = isDarkMode.value
 
         // Listen for changes in the dark mode preference
         val listener: (dynamic) -> Unit = { event ->
             isDarkMode.value = event.matches
+            latestDarkMode = event.matches
         }
         darkModeMediaQuery.addListener(listener)
 
